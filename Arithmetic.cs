0ff4baf934ac31e9e6c410b60ea96cb1f5e979f7
ogodwin <oliver.godwin3414@omail.otech.edu>
2022-03-16 13:15:49 -0600

    Added arithmetic.cs

--- Arithmetic.cs
+++ Arithmetic.cs
@@ -1,2 +1,34 @@
-using system;
-class Addition{}
+using System;
+
+namespace A2_4.Arithmetic
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number1;
+            int number2;
+            int sum;
+            int difference;
+            int product;
+            int quotient;
+
+            Console.Write("Enter first integer: ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+            difference = number1 - number2;
+            product = number1 * number2;
+            quotient = number1 / number2;
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Difference = " + difference);
+            Console.WriteLine("Product = " + product);
+            Console.WriteLine("Quotient = " + quotient);
+
+        }
+    }
+}
